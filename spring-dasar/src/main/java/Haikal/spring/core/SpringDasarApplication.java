1470aa07a2d67dd94841469e1b90cf964bc91cfa
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-07-26 10:06:28 +0700

    Dasar - Inversion

---
+++ spring-dasar/src/main/java/Haikal/spring/core/SpringDasarApplication.java
@@ -0,0 +1,13 @@
+package Haikal.spring.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDasarApplication.class, args);
+	}
+
+}
---
+++ spring-dasar/src/test/java/Haikal/spring/core/SpringDasarApplicationTests.java
@@ -0,0 +1,13 @@
+package Haikal.spring.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
