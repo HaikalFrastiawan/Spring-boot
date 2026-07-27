85e52a67426062e14a8c47ac9f6ff5dda16a3376
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-07-28 06:49:32 +0700

    Dasar - Circular Dependency

---
+++ spring-dasar/src/main/java/Haikal/spring/core/data/cyclic/CyclicC.java
@@ -0,0 +1,11 @@
+package Haikal.spring.core.data.cyclic;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CyclicC {
+
+    private CyclicA cyclicA;
+}
