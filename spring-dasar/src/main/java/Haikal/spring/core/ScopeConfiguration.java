f782ad55e180715e46f0b807395a0f2bf336aee8
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-07-30 07:12:34 +0700

    Dasar - membuat scope

--- spring-dasar/src/main/java/Haikal/spring/core/ScopeConfiguration.java
+++ spring-dasar/src/main/java/Haikal/spring/core/ScopeConfiguration.java
@@ -1,7 +1,10 @@
 package Haikal.spring.core;
 
+import Haikal.spring.core.data.Bar;
 import Haikal.spring.core.data.Foo;
+import Haikal.spring.core.scope.DoubletonScope;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -16,4 +19,19 @@ public class ScopeConfiguration {
         log.info("Create New Foo");
         return new Foo();
     }
+
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer(){
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        configurer.addScope("doubleton",new DoubletonScope());
+        return configurer;
+    }
+
+    @Bean
+    @Scope("doubleton")
+    public Bar bar(){
+        log.info("Create new bar");
+        return new Bar();
+    }
+
 }
