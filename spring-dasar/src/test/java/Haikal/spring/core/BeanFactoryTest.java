811a1b0df862a8ff109c846c4aa72dbd752e1357
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-08-16 14:06:00 +0700

    Dasar - Bean Factoryy

---
+++ spring-dasar/src/test/java/Haikal/spring/core/BeanFactoryTest.java
@@ -0,0 +1,33 @@
+package Haikal.spring.core;
+
+import Haikal.spring.core.data.Foo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class BeanFactoryTest {
+    private ConfigurableApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(ScanConfiguration.class);
+        applicationContext.registerShutdownHook();
+    }
+
+    @Test
+    void testBeanFactory() {
+        ObjectProvider<Foo> fooObjectProvider = applicationContext.getBeanProvider(Foo.class);
+        List<Foo> list = fooObjectProvider.stream().collect(Collectors.toList());
+
+        System.out.println(list);
+
+        Map<String, Foo> beans = applicationContext.getBeansOfType(Foo.class);
+        System.out.println(beans);
+    }
+}
