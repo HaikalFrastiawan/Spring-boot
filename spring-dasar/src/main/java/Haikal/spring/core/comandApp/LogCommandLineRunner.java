91f966e411e87a362264bf78281fc6ecae75444a
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-08-20 18:55:25 +0700

    Dasar - CommandLineRunner

---
+++ spring-dasar/src/main/java/Haikal/spring/core/comandApp/LogCommandLineRunner.java
@@ -0,0 +1,20 @@
+package Haikal.spring.core.comandApp;
+
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+@Slf4j
+@Component
+public class LogCommandLineRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("log command line runner : {}", Arrays.toString(args));
+
+    }
+}
