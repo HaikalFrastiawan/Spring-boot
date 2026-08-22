63f75b57767ad300c60242a6eefdc14384022b1d
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-08-22 14:04:29 +0700

    WebMVC -  Servlet Request dan Response

--- spring-webmvc/src/main/java/Spring/Web/MVC/controller/HelloController.java
+++ spring-webmvc/src/main/java/Spring/Web/MVC/controller/HelloController.java
@@ -1,17 +1,23 @@
 package Spring.Web.MVC.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @Controller
 public class HelloController {
 
     @RequestMapping(path = "hello")
-    public void HelloWorld(HttpServletResponse response) throws IOException {
-        response.getWriter().println("Hello World");
+    public void HelloWorld(HttpServletResponse response, HttpServletRequest request) throws IOException {
+        String name = request.getParameter("name");
+        if (Objects.isNull(name)) {
+            name = "guest";
+        }
+        response.getWriter().println("Hello World" + name );
 
     }
 }
