794b3f9b31906e1bedccc131716a9723b97b6390
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-08-22 14:43:54 +0700

    WebMVC -  Mock MVC

--- spring-webmvc/src/main/java/Spring/Web/MVC/controller/HelloController.java
+++ spring-webmvc/src/main/java/Spring/Web/MVC/controller/HelloController.java
@@ -1,23 +1,23 @@
-package Spring.Web.MVC.controller;
+    package Spring.Web.MVC.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+    import jakarta.servlet.http.HttpServletRequest;
+    import jakarta.servlet.http.HttpServletResponse;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
-import java.util.Objects;
+    import java.io.IOException;
+    import java.util.Objects;
 
-@Controller
-public class HelloController {
+    @Controller
+    public class HelloController {
 
-    @RequestMapping(path = "hello")
-    public void HelloWorld(HttpServletResponse response, HttpServletRequest request) throws IOException {
-        String name = request.getParameter("name");
-        if (Objects.isNull(name)) {
-            name = "guest";
+        @RequestMapping(path = "hello")
+        public void HelloWorld(HttpServletResponse response, HttpServletRequest request) throws IOException {
+            String name = request.getParameter("name");
+            if (Objects.isNull(name)) {
+                name = "Guest";
+            }
+            response.getWriter().println("Hello " + name );
+
         }
-        response.getWriter().println("Hello World" + name );
-
     }
-}
---
+++ spring-webmvc/src/test/java/Spring/Web/MVC/HelloControllerTest.java
@@ -0,0 +1,39 @@
+package Spring.Web.MVC;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMVC;
+
+    @Test
+    void helloGuest() throws Exception {
+        mockMVC.perform(
+                get("/hello")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("Hello Guest"))
+        );
+    }
+
+    @Test
+    void helloName() throws Exception {
+        mockMVC.perform(
+                get("/hello").param("name", "Haikal")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("Hello Haikal"))
+        );
+    }
+}
