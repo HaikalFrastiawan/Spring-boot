d203350cc434b87caf0ee6900c3bb2d26ca87b73
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-08-24 06:57:11 +0700

    WebMVC - Response Status

---
+++ spring-webmvc/src/test/java/Spring/Web/MVC/controller/CodeControllerTest.java
@@ -0,0 +1,33 @@
+package Spring.Web.MVC.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CodeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void deleteProduct() throws Exception {
+        mockMvc.perform(
+                delete("/products/1")
+        ).andExpectAll(
+                status().isAccepted()
+        );
+    }
+}
