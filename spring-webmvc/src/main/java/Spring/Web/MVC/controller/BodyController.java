50846a4d80142c55a2644d1bcb226316412dc76a
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-08-24 06:49:20 +0700

    WebMVC -  Request Body

---
+++ spring-webmvc/src/main/java/Spring/Web/MVC/controller/BodyController.java
@@ -0,0 +1,38 @@
+package Spring.Web.MVC.controller;
+
+
+import Spring.Web.MVC.model.HelloRequest;
+import Spring.Web.MVC.model.HelloResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import tools.jackson.databind.ObjectMapper;
+
+
+
+@Controller
+public class BodyController {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @PostMapping(
+            path = "/body/hello",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+
+    @ResponseBody
+    public String Body(@RequestBody String requestBody) throws Exception{
+        HelloRequest request = objectMapper.readValue(requestBody, HelloRequest.class);
+
+        HelloResponse response = new HelloResponse();
+        response.setHello("Hello " + request.getName());
+
+        return objectMapper.writeValueAsString(response);
+    }
+
+}
---
+++ spring-webmvc/src/main/java/Spring/Web/MVC/model/HelloRequest.java
@@ -0,0 +1,13 @@
+package Spring.Web.MVC.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloRequest {
+
+    private String name;
+}
