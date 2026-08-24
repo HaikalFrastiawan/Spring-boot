a74f7d2c27eff0a93162cb730f437f733f6ff798
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-08-24 18:55:00 +0700

    WebMVC - Response Entity

---
+++ spring-webmvc/src/test/java/Spring/Web/MVC/controller/AuthControllerTest.java
@@ -0,0 +1,51 @@
+package Spring.Web.MVC.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void loginSucess() throws Exception {
+        mockMvc.perform(
+                post("/auth/login")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("username", "haikal")
+                        .param("password", "rahasia")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("OK"))
+        );
+    }
+
+    @Test
+    void loginFailed() throws Exception {
+        mockMvc.perform(
+                post("/auth/login")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("username", "haikal")
+                        .param("password", "salah")
+        ).andExpectAll(
+                status().isUnauthorized(),
+                content().string(Matchers.containsString("KO"))
+        );
+    }
+}
