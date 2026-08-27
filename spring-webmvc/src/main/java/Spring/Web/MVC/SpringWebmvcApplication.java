fe24ab06fe89740538778ffc69f3c2e86c1e1917
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-08-27 19:30:26 +0700

    DataJpa - Data Source

--- spring-webmvc/src/main/java/Spring/Web/MVC/SpringWebmvcApplication.java
+++ spring-webmvc/src/main/java/Spring/Web/MVC/SpringWebmvcApplication.java
@@ -2,6 +2,8 @@ package Spring.Web.MVC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringWebmvcApplication {
