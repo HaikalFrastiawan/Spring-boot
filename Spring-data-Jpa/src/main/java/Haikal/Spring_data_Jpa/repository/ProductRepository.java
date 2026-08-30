a3029b473658f226404a26d9888f04cd64308026
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-08-30 10:36:52 +0700

    DataJpa - Query Relation

---
+++ Spring-data-Jpa/src/main/java/Haikal/Spring_data_Jpa/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package Haikal.Spring_data_Jpa.repository;
+
+import Haikal.Spring_data_Jpa.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAllByCategory_Name(String name);
+}
---
+++ Spring-data-Jpa/src/test/java/Haikal/Spring_data_Jpa/repository/ProductRepositoryTest.java
@@ -0,0 +1,49 @@
+package Haikal.Spring_data_Jpa.repository;
+
+import Haikal.Spring_data_Jpa.entity.Category;
+import Haikal.Spring_data_Jpa.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProductRepositoryTest {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void createProduct() {
+        Category category = categoryRepository.findById(1L).orElse(null);
+        assertNotNull(category);
+        {
+            Product product = new Product();
+            product.setName("Apple iphone 14");
+            product.setPrice(25_000_000L);
+            product.setCategory(category);
+            productRepository.save(product);
+        }
+        {
+            Product product = new Product();
+            product.setName("Apple iphone 13");
+            product.setPrice(18_000_000L);
+            product.setCategory(category);
+            productRepository.save(product);
+        }
+
+    }
+
+    @Test
+    void findByCategoryName() {
+        List<Product> product = productRepository.findAllByCategory_Name("Gadget Murah");
+        assertEquals(2, product.size());
+        assertEquals("Apple iphone 14", product.get(0).getName());
+        assertEquals("Apple iphone 13", product.get(1).getName());
+    }
+}
