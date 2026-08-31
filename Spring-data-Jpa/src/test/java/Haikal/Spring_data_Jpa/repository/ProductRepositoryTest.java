2398d845483b9e37d92da85365f5f30fedff813b
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-08-31 07:07:13 +0700

    DataJpa - Page Result

--- Spring-data-Jpa/src/test/java/Haikal/Spring_data_Jpa/repository/ProductRepositoryTest.java
+++ Spring-data-Jpa/src/test/java/Haikal/Spring_data_Jpa/repository/ProductRepositoryTest.java
@@ -5,6 +5,7 @@ import Haikal.Spring_data_Jpa.entity.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -64,14 +65,20 @@ class ProductRepositoryTest {
 
         //page 0
         Pageable pageable = PageRequest.of(0, 1, Sort.by(Sort.Order.desc("id")));
-        List<Product> products = productRepository.findAllByCategory_Name("Gadget Murah", pageable);
-        assertEquals(1, products.size());
-        assertEquals("Apple iphone 13", products.get(0).getName());
+        Page<Product> products = productRepository.findAllByCategory_Name("Gadget Murah", pageable);
+        assertEquals(1, products.getContent().size());
+        assertEquals(0, products.getNumber());
+        assertEquals(2, products.getTotalElements());
+        assertEquals(2, products.getTotalPages());
+        assertEquals("Apple iphone 13",products.getContent().get(0).getName());
 
-        //page 2
+        //page 1
         pageable = PageRequest.of(1, 1, Sort.by(Sort.Order.desc("id")));
         products = productRepository.findAllByCategory_Name("Gadget Murah", pageable);
-        assertEquals(1, products.size());
-        assertEquals("Apple iphone 14", products.get(0).getName());
+        assertEquals(1, products.getContent().size());
+        assertEquals(1, products.getNumber());
+        assertEquals(2, products.getTotalElements());
+        assertEquals(2, products.getTotalPages());
+        assertEquals("Apple iphone 14",products.getContent().get(0).getName());
     }
 }
