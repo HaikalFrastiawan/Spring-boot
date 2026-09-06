a5e40cd46a1b7ad35830167c2cf4dc97cf170dc4
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-09-07 06:09:30 +0700

    monitoring - Micrometer

--- spring-monitoring/src/main/java/Haikal/spring_monitoring/MySchedulTask.java
+++ spring-monitoring/src/main/java/Haikal/spring_monitoring/MySchedulTask.java
@@ -1,5 +1,7 @@
 package Haikal.spring_monitoring;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -7,8 +9,13 @@ import java.util.concurrent.TimeUnit;
 
 @Component
 public class MySchedulTask {
+
+    @Autowired
+    private MeterRegistry meterRegistry;
+
     @Scheduled(fixedRate = 10L, timeUnit = TimeUnit.SECONDS)
     public void hello(){
+        meterRegistry.counter("my.scheduleTask").increment();
         System.out.println("Hello World");
     }
 }
