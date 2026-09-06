d276e5205bf6c0e696e737bac7d9295011e78536
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-09-07 05:56:15 +0700

    monitoring - ScheduleTask

--- spring-monitoring/src/main/java/Haikal/spring_monitoring/SpringMonitoringApplication.java
+++ spring-monitoring/src/main/java/Haikal/spring_monitoring/SpringMonitoringApplication.java
@@ -2,8 +2,10 @@ package Haikal.spring_monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringMonitoringApplication {
 
 	public static void main(String[] args) {
