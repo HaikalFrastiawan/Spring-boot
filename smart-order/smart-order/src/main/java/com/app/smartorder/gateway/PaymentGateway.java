bead2a84283f2f2ae2bd3e90763dd01fb25a513a
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-09-07 19:23:09 +0700

    restful - address Api

--- smart-order/smart-order/src/main/java/com/app/smartorder/gateway/PaymentGateway.java
+++
@@ -1,8 +0,0 @@
-package com.app.smartorder.gateway;
-
-import java.math.BigDecimal;
-
-public interface PaymentGateway {
-    boolean proccessPayment(String orderId, BigDecimal amount);
-    String getGatewayName();
-}
